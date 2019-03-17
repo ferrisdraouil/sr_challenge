a63d46381edf569961493325dedccc62b3ad8c57
ferrisdraouil <fdraouil@gmail.com>
2019-03-16 20:54:25 -0700

    built endpoints

--- app.js
+++ app.js
@@ -6,4 +6,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-ByeWeek.loadSeasonData(2014, 'REG')
+app.get("/byeweek/:teamHandle/:year", async function(req, res, next) {
+  try {
+    const { teamHandle, year } = req.params
+    const byeWeek = await ByeWeek.findByeWeek(teamHandle, year, 'REG');
+    return res.json(byeWeek);
+  } catch (e) {
+    return new Error(e);
+  }
+});
+
+app.get("/postbyeaverages/:teamHandle/:year/:period?", async function(req, res, next) {
+  try {
+    const { teamHandle, year, period } = req.params
+    const byeWeek = await ByeWeek.findPostByeAverageScores(teamHandle, year, period);
+    return res.json(byeWeek);
+  } catch (e) {
+    return new Error(e);
+  }
+});
+
+module.exports = app;
