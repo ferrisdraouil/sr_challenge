a0f63d78124c9a53077aec103d00a4cef94c8416
ferrisdraouil <fdraouil@gmail.com>
2019-03-16 20:55:31 -0700

    added tests for routes

--- ByeWeek.test.js
+++ ByeWeek.test.js
@@ -1,8 +1,9 @@
-process.env.NODE_ENV = "test"
-
 const BASE_URL = 'https://api.ngs.nfl.com/league/schedule';
 const ByeWeek = require('./ByeWeek')
 const db = require('./db');
+const request = require("supertest");
+const app = require("./app")
+
 
 let gameData
 let balHomeGames
@@ -41,11 +42,42 @@ describe("updatePointsAfterBye", function() {
   test("it should correctly update additional games", async function() {
     let secondQuarterSumAfterTwoGames = balHomeGames[0].score.homeTeamScore.pointQ2 + balHomeGames[1].score.homeTeamScore.pointQ2
     ByeWeek.updatePointsAfterBye(emptyPointsObj, 'BAL', 'home', balHomeGames[1])
-    await ByeWeek.loadSeasonData(2018, 'REG')
+    await ByeWeek.loadSeasonData(2014, 'REG')
     expect(emptyPointsObj['BAL'].otGames).toEqual(0)
     expect(emptyPointsObj['BAL'].pointQ2).toEqual(secondQuarterSumAfterTwoGames)
   })
 })
+
+describe("GET /byeweek", function () {
+  test("Returns correct data", async function () {
+    const philadelphia = await request(app).get('/byeweek/PHI/2018');
+    const carolina = await request(app).get('/byeweek/CAR/2018');
+    expect(philadelphia.statusCode).toBe(200);
+    expect(philadelphia.body[0].week).toBe(9);
+    expect(carolina.statusCode).toBe(200);
+    expect(carolina.body[0].week).toBe(4);
+  });
+});
+
+describe("GET /postbyeaverages", function () {
+  test("Returns correct data without passing period", async function () {
+    const philadelphia = await request(app).get('/postbyeaverages/PHI/2018');
+    const carolina = await request(app).get('/postbyeaverages/CAR/2018');
+    expect(philadelphia.statusCode).toBe(200);
+    expect(philadelphia.body[0].total_avg).toBe(23.63);
+    expect(carolina.statusCode).toBe(200);
+    expect(carolina.body[0].total_avg).toBe(23.46);
+  });
+
+  test("Returns correct data when passing period", async function () {
+    const philadelphia = await request(app).get('/postbyeaverages/PHI/2018/first_quarter');
+    const carolina = await request(app).get('/postbyeaverages/CAR/2018/first_quarter');
+    expect(philadelphia.statusCode).toBe(200);
+    expect(philadelphia.body[0].first_quarter).toBe(2.5);
+    expect(carolina.statusCode).toBe(200);
+    expect(carolina.body[0].first_quarter).toBe(6.62);
+  });
+});
 
 
 
